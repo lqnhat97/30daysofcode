9b116f09cd16c7c3512554173246d8b4e3382a78
nhat.le <nhat.le@tpptechnology.com>
2023-08-06 21:37:08 +0700

    Update Day 18

---
+++ src/test/java/nhat/le/Day18Test.java
@@ -0,0 +1,13 @@
+package nhat.le;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Day18Test {
+
+    @Test
+    void solve() {
+        String str = "racecar";
+        Assertions.assertTrue(Day18.solve(str));
+    }
+}
